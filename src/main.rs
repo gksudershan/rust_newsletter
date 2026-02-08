38d66ebdc24a80608b09afd3b5239c65e9d3c789
Sudershan <gksudershan@gmail.com>
2026-02-08 18:48:14 +0530

    added notes

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 /// * Run tests
 /// * Run the application
 
-
 // okay.. there are two parts - i.e two user stories
 // 1. for the user to be able to subscribe to the service with their mail id
 // 2. for the author to be able to send mails to the users
