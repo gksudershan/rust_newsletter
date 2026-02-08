70253c397e4bca77dbe122be6237839945947065
Sudershan <gksudershan@gmail.com>
2026-02-08 18:39:49 +0530

    added notes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+///Inner dev loop
+/// * Make changes
+/// * Compile the application
+/// * Run tests
+/// * Run the application
+
+
+// okay.. there are two parts - i.e two user stories
+// 1. for the user to be able to subscribe to the service with their mail id
+// 2. for the author to be able to send mails to the users
+// both of these are meant to announce/to be aware when new content is published
 fn main() {
     println!("Hello, world!");
 }
